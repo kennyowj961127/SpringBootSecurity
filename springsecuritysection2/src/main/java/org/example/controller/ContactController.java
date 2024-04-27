cae2f0196b3318b6230d67c4f5672bf360590020
kennyowj961127 <123232019+kennyowj961127@users.noreply.github.com>
2024-04-27 16:00:59 +0800

    Added Project Security Config
    Security Filter Chain example

---
+++ springsecuritysection2/src/main/java/org/example/controller/ContactController.java
@@ -0,0 +1,12 @@
+package org.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContactController {
+    @GetMapping("/contact")
+    public String getContactDetails() {
+        return "My Contact Details from DB";
+    }
+}

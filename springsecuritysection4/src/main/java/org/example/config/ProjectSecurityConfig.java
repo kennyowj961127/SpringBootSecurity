32e7f7fe10a090477e71fec4392720fdbff712c4
kennyowj961127 <123232019+kennyowj961127@users.noreply.github.com>
2024-04-29 01:34:12 +0800

    Init section 4

---
+++ springsecuritysection4/src/main/java/org/example/config/ProjectSecurityConfig.java
@@ -0,0 +1,77 @@
+package org.example.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import javax.sql.DataSource;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((requests)->requests
+                .requestMatchers("/myAccount", "myBalance", "myLoans", "myCards").authenticated()
+                .requestMatchers("/notices", "/contact", "/register").permitAll())
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults());
+
+        return http.build();
+    }
+
+    /*
+    *   This method is used to create a user with the username "admin" and password "admin" with the role "admin"
+    */
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService(){
+////        Approach 1 - Using User.withDefaultPasswordEncoder()
+////        UserDetails admin = User.withDefaultPasswordEncoder()
+////                .username("admin")
+////                .password("admin")
+////                .authorities("admin")
+////                .build();;
+////
+////        UserDetails user = User.withDefaultPasswordEncoder()
+////                .username("user")
+////                .password("user")
+////                .authorities("read")
+////                .build();;
+////
+////        return new InMemoryUserDetailsManager(admin, user);
+//
+////        Approach 2 - Using NoOpPasswordEncoder
+//        UserDetails admin = User.withUsername("admin")
+//                .password("admin")
+//                .authorities("admin")
+//                .build();
+//
+//        UserDetails user = User.withUsername("user")
+//                .password("user")
+//                .authorities("read")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(admin, user);
+//    }
+
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource dataSource){
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+       return NoOpPasswordEncoder.getInstance();
+    }
+}

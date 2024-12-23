823a4fa047de0ccfa36ef4b60f6c3a11e39aa9c6
Kodebug-2024 <kodebug2024@gmail.com>
2024-12-23 23:54:02 +0800

    Added New Section 7

---
+++ springsecuritysection7-NewVersionFrom5/src/main/java/org/example/controller/CardsController.java
@@ -0,0 +1,12 @@
+package org.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CardsController {
+    @GetMapping("/myCards")
+    public String getCardDetails() {
+        return "My Card Details from DB";
+    }
+}

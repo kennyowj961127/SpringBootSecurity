36358ff79cac45f4a3508636fd29bdb6f872e97f
Kodebug-2024 <kodebug2024@gmail.com>
2025-01-01 22:45:41 +0800

    Added Section 8 from Section 7

---
+++ springsecuritysection8-NewVersionFrom7/src/main/java/org/example/config/ProjectSecurityConfig.java
@@ -0,0 +1,50 @@
+package org.example.config;
+
+import org.example.exception.CustomAccessDeniedHandler;
+import org.example.exception.CustomBasicAuthenticationEntryPoint;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@Profile("!prod")
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.sessionManagement((sessionManagement) -> sessionManagement.invalidSessionUrl("/invalidSession").maximumSessions(3).maxSessionsPreventsLogin(true))
+//                .sessionManagement((sessionManagement) -> sessionManagement.sessionFixation((SessionManagementConfigurer.SessionFixationConfigurer::newSession)))
+                .requiresChannel((channel) -> channel.anyRequest().requiresInsecure())
+                .csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((requests)->requests
+                .requestMatchers("/myAccount", "myBalance", "myLoans", "myCards").authenticated()
+                .requestMatchers("/notices", "/contact","/error", "/register", "/invalidSession").permitAll())
+                .formLogin(withDefaults())
+                .httpBasic(httpBasic -> httpBasic.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()))
+                .exceptionHandling((exceptionHandling) -> exceptionHandling.accessDeniedHandler(new CustomAccessDeniedHandler()));
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    /**
+     * From Spring Security 6.3 version
+     * @return CompromisedPasswordChecker
+     */
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
+    }
+}

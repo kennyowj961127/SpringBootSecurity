91b14073a7424b30f67d64218b177ceac9f10de4
Kodebug-2024 <kodebug2024@gmail.com>
2025-01-08 22:51:57 +0800

    EnableWebSecurity

--- springsecuritysection10-NewVersionFrom9/src/main/java/org/example/KennyBankBackendApplication.java
+++ springsecuritysection10-NewVersionFrom9/src/main/java/org/example/KennyBankBackendApplication.java
@@ -2,8 +2,10 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity(debug = true)
 public class KennyBankBackendApplication {
 
 	public static void main(String[] args) {
